28fc87a1593a56ea3dea9baa185700f0ef524705
dmqkuku <wgtr9512@gmail.com>
2022-04-17 16:18:41 +0900

    20220412

--- src/main/java/com/home/jhshome/app/publicSpace/PublicSpaceController.java
+++ src/main/java/com/home/jhshome/app/publicSpace/PublicSpaceController.java
@@ -1,6 +1,7 @@
 package com.home.jhshome.app.publicSpace;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,20 @@ public class PublicSpaceController {
     public String welcome(Model model){
 
         return "publicSpace/welcome";
+    }
+    @MessageMapping("/welcome/ws")
+    @SendTo("publicSpace/welcome")
+    public WsResponse welcomewe(){
+        return new WsResponse(new Date(), "Welcome! to WS!");
+    }
+    private class WsResponse{
+        private Date serverTime;
+        private String msg;
+
+        private WsResponse(Date serverTime, String msg){
+            this.serverTime = serverTime;
+            this.msg = msg;
+        }
     }
 
 }
